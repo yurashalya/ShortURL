b4e7c0f6ee3c6012e48c0967df39ead3f01289c6
YuraS <stallker9696@gmail.com>
2021-01-23 03:46:50 +0200

    Initial project + connect with MongoDB

---
+++ app.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const config = require("config");
+const mongoose = require("mongoose");
+
+const app = express();
+
+const PORT = config.get("port") || 5000;
+
+async function start() {
+  try {
+    await mongoose.connect(config.get("mongoUrl"), {
+      user: config.get("user"),
+      pass: config.get("pass"),
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+    });
+    app.listen(PORT, () => {
+      console.log(`App has benn started on port ${PORT}...`);
+    });
+  } catch (e) {
+    console.log("Server Error", e);
+    process.exit(1);
+  }
+}
+
+start();
