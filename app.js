ae533fbaa72fe30763eafc3656487041ed0ad811
YuraS <stallker9696@gmail.com>
2021-01-26 00:05:08 +0200

    Created routers logic for registration and login

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const mongoose = require("mongoose");
 
 const app = express();
 
+app.use("/api/auth", require("./routes/auth.routes"));
+
 const PORT = config.get("port") || 5000;
 
 async function start() {
---
+++ routes/auth.routes.js
@@ -0,0 +1,89 @@
+const { Router } = require("express");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const { check, validationResult } = require("express-validator");
+const User = require("../models/User");
+const config = require("config");
+const router = Router();
+
+router.post(
+  "/register",
+  [
+    check("email", "Incorrect email").isEmail(),
+    check("password", "Min length 6 symbol").isLength({ min: 6 }),
+  ],
+  async (req, res) => {
+    try {
+      const errors = validationResult(req);
+
+      if (!errors.isEmpty()) {
+        return res.status(400).json({
+          errors: errors.array(),
+          message: "Invalid values for registretion",
+        });
+      }
+
+      const { email, password } = res.body;
+
+      const candidate = await User.findOne({ email });
+
+      if (candidate) {
+        return res.status(400).json({ message: "User name already exists" });
+      }
+
+      const hashedPassword = await bcrypt.hash(password, 12);
+      const user = new User({ email, password: hashedPassword });
+
+      await user.save();
+      res.status(201).json({ message: "User created" });
+    } catch (e) {
+      res.status(500).json({ message: "Something went wrong" });
+    }
+  }
+);
+
+router.post(
+  "/login",
+  [
+    check("email", "Please enter correct email").normalizeEmail().isEmail(),
+    check("password", "Please enter correct password").exists(),
+  ],
+  async (req, res) => {
+    try {
+      const errors = validationResult(req);
+
+      if (!errors.isEmpty()) {
+        return res.status(400).json({
+          errors: errors.array(),
+          message: "Invalid values ​​for at login",
+        });
+      }
+
+      const { email, password } = res.body;
+
+      const user = await User.findOne({ email });
+
+      if (!user) {
+        return res.status(400).json({ message: "User is not found" });
+      }
+
+      const isCorrectPassword = await bcrypt.compare(password, user.password);
+
+      if (!isCorrectPassword) {
+        return res
+          .status(400)
+          .json({ message: "Ops..., Something went wrong, try again" });
+      }
+
+      const token = jwt.sign({ userId: user.id }, config.get("jwtSecret"), {
+        expiresIn: "1h",
+      });
+
+      res.json({ token, userId: user.id });
+    } catch (e) {
+      res.status(500).json({ message: "Something went wrong" });
+    }
+  }
+);
+
+module.exports = router;
